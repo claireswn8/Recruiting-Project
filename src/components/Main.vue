81bd853baae67a5d22436b022e1a11ab7a8c5f64
C. I. Swanson <swanscol@oregonstate.edu>
2022-10-04 10:41:55 -0700

    Code cleanup and add simple validation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3 v-if="guestNumber >= 0">Total Guests: {{guestNumber}}/20</h3>
+    <h3 v-if="guestTotal >= 0">Total Guests: {{guestTotal}}/20</h3>
     <table>
       <thead>
         <th>Email</th>
@@ -8,26 +8,26 @@
         <th></th>
         <th></th>
       </thead>
-      <div id="loading_text" v-if="guestNumber < 0">
+      <div id="loading_text" v-if="guestTotal < 0">
         <h2>Loading Data...</h2>
       </div>
       <tbody>  
         <tr v-for="(guest, index) in this.guests" :key="index">
           <td>{{guest.email}}</td>
           <td>{{guest.tickets}}</td>
-          <td><button :disabled="buttonsDisabled == 1" v-on:click="handleEditClick(index)">Edit</button></td>
-          <td><button :disabled="buttonsDisabled == 1" v-on:click="handleDeleteClick(index)">Delete</button></td>
+          <td><button :disabled="buttonsDisabled === 1" v-on:click="handleEditClick(index)">Edit</button></td>
+          <td><button :disabled="buttonsDisabled === 1" v-on:click="handleDeleteClick(index)">Delete</button></td>
         </tr>      
-        <tr id="add_guest_row" v-if="guestNumber < 20">
+        <tr id="add_guest_row" v-if="guestTotal < 20">
           <td><input v-model="addValues.email" placeholder="Email"></td>
           <td><input v-model="addValues.tickets" placeholder="Tickets"></td>
-          <td><button :disabled="buttonsDisabled == 1" v-on:click="handleAddClick()">Add</button></td>
+          <td><button :disabled="buttonsDisabled === 1" v-on:click="handleAddClick()">Add</button></td>
           <td></td>
         </tr>
       </tbody>
     </table>
     
-    <div v-if="editModalHidden == 0" id="edit_modal">
+    <div v-if="editModalHidden === 0" id="edit_modal">
       <div>Edit Guest Information</div>    
       <div> 
         Email 
@@ -43,7 +43,7 @@
       </div>
     </div>
 
-    <div v-if="editModalHidden == 0" v-on:click="handleCancelClick()" id="page_mask"></div>
+    <div v-if="editModalHidden === 0" v-on:click="handleCancelClick()" id="page_mask"></div>
 
   </div>
 </template>
@@ -64,7 +64,7 @@
   export default {
     data: () => {
         return { 
-          guests: [], guestNumber: -1, addValues: new Guest(), 
+          guests: [], guestTotal: -1, addValues: new Guest(), 
           editValues: new Guest(), editIndex: 0, editModalHidden: 1, 
           buttonsDisabled: 0 
         };
@@ -78,11 +78,19 @@
          */
         async loadGuests() {
           this.guests = await guestRepo.load();
-          this.guestNumber = this.guests.length;
+          this.guestTotal = this.guests.length;
+        },
+        /**
+         * Save guest list to repo; disable edit/save/delete buttons until save completes
+         */
+         async saveToGuestRepo() {
+          this.buttonsDisabled = 1;
+          await guestRepo.save(this.guests);
+          this.buttonsDisabled = 0;
         },
         /**
          * Open guest edit modal and populate input values
-         * @param {*} index - guest index to edit
+         * @param {*} index - guests list index to use
          */
         handleEditClick(index) {
           this.editModalHidden = 0;
@@ -94,6 +102,16 @@
          * Update guest from edit modal inputs and close modal
          */
         async handleSaveClick() {
+          //Validate edited guest
+          try {
+            await this.validateGuest(this.editValues)
+          }
+          catch(e) {
+            alert(e.message);
+            return;
+          }
+
+          //Save edited guest
           this.guests[this.editIndex].email = this.editValues.email;
           this.guests[this.editIndex].tickets = this.editValues.tickets;
           this.editModalHidden = 1;
@@ -107,29 +125,46 @@
         },
         /**
          * Delete guest from guest list
-         * @param {int} index - guest index to delete
+         * @param {int} index - guests list index to delete
          */
         async handleDeleteClick(index) {
           this.guests.splice(index, 1);
-          this.guestNumber = this.guestNumber - 1;
+          this.guestTotal = this.guestTotal - 1;
           this.saveToGuestRepo();
         },
         /**
          * Create new guest entry from add inputs
          */
         async handleAddClick() {
-          this.guestNumber = this.guests.push({email: this.addValues.email, tickets: this.addValues.tickets});
+          //Validate new guest
+          try {
+            await this.validateGuest(this.addValues)
+          }
+          catch(e) {
+            alert(e.message);
+            return;
+          }
+
+          //Save new guest
+          this.guestTotal = this.guests.push({email: this.addValues.email, tickets: this.addValues.tickets});
           this.addValues.email = "";
           this.addValues.tickets = "";
           this.saveToGuestRepo();
         },
         /**
-         * Save guest list to repo; disable edit/save/delete buttons until save completes
+         * Validates guest. Tickets must be a positive integer and email must include an @. 
+         * Async to allow for possible future API validation; will never be added to this, but as a hypothetical. 
+         * @param {*} guest - Guest to validate
          */
-        async saveToGuestRepo() {
-          this.buttonsDisabled = 1;
-          await guestRepo.save(this.guests);
-          this.buttonsDisabled = 0;
+        async validateGuest(guest) {
+          const tickets = Number(guest.tickets);
+          if(Number.isNaN(tickets) || !Number.isInteger(tickets) || tickets < 0){
+            throw new Error("Invalid Tickets; Tickets must be a positive integer");
+          }
+
+          if(!guest.email.includes("@")){
+            throw new Error("Invalid Email; Email must contain an @");
+          }
         },
     },
 }
@@ -190,17 +225,16 @@
 
   /*Modal Styling*/
   #edit_modal {
-  border: 1px solid black;
-  z-index: 1;
-  width: 40%;
-  height: 40%;
-  position: absolute;
-  left: 30%;
-  top: 10%;
-  box-sizing: border-box;
-  border-radius: 5px;
-  display: inline-block;
-  background-color: white;
+    border: 1px solid black;
+    z-index: 1;
+    width: 40%;
+    height: 40%;
+    position: absolute;
+    left: 30%;
+    top: 10%;
+    box-sizing: border-box;
+    border-radius: 5px;
+    background-color: white;
   }
 
   #edit_modal div {
@@ -237,5 +271,6 @@
   #loading_text {
     text-align: center;
   }
+
 
 </style>
